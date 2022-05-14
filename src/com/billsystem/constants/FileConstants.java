f08158036e27e35afbbf618a29f2bf4820063deb
GOWTHAMAN R <61967645+rgowthaman@users.noreply.github.com>
2022-05-14 19:07:02 +0530

    Update

--- src/com/billsystem/constants/FileConstants.java
+++ src/com/billsystem/constants/FileConstants.java
@@ -1,8 +1,11 @@
 package com.billsystem.constants;
 
+// import java.io.File;
+
 public class FileConstants {
-    public static final String FILE_DIR = System.getProperty("user.dir");
     public static final String FILE_SEPARATOR = "/";
+    public static final String FILE_DIR = System.getProperty("user.dir") + FILE_SEPARATOR + "Json";
+    //    public static final String FILE_DIR = new File(FileConstants.class.getResource("/").getPath(), "Json").toString();
     public static final String USER_JSON_FILE = "user.json";
     public static final String ITEM_JSON_FILE = "item.json";
     public static final String ORDER_JSON_FILE = "order.json";
